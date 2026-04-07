c1e0534a967de50c69702e274b3f520085e00695
Saku <saku@shiina.xyz>
2026-04-07 21:58:11 +0530

    fix: move hono app to root app.js for vercel detection

--- dev.js
+++ dev.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { serve } from "@hono/node-server";
-import app from "./api/app.js";
+import app from "./app.js";
 
 const port = Number(process.env.PORT ?? process.env.port ?? 9000);
 serve({ fetch: app.fetch, port, hostname: "0.0.0.0" }, (info) => {
